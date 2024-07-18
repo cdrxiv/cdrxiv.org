c5b77d3791d8bcdf589da309e96c4577808dfe9f
katamartin <katasm@gmail.com>
2024-07-18 15:15:37 -0400

    Also add signout button

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -1,17 +1,42 @@
 'use client'
 
 import { Box, Flex } from 'theme-ui'
-import { useSession, signIn } from 'next-auth/react'
-import StyledButton from '../../components/button'
+import { useState } from 'react'
+import { useSession, signIn, signOut } from 'next-auth/react'
+
+import StyledLink from '../../components/link'
 
 const UserInfo = () => {
+  const [expanded, setExpanded] = useState(false)
   const { data: session, status } = useSession()
 
   if (status === 'authenticated' && session) {
-    return <p>Signed in as {session?.user?.email}</p>
+    return (
+      <Box sx={{ position: 'relative' }}>
+        <StyledLink onClick={() => setExpanded(!expanded)} showArrow={false}>
+          Signed in as {session?.user?.email ?? 'Unknown'}{' '}
+          <Box
+            sx={{
+              display: 'inline-block',
+              transform: expanded ? 'rotate(90deg)' : 'rotate(0deg)',
+              transition: 'transform 0.2s',
+            }}
+          >
+            {'>>'}
+          </Box>
+        </StyledLink>
+        {expanded && (
+          <Box sx={{ position: 'absolute', top: '28px' }}>
+            <StyledLink onClick={() => signOut()} showArrow={false}>
+              Sign out
+            </StyledLink>
+          </Box>
+        )}
+      </Box>
+    )
   }
 
-  return <StyledButton onClick={() => signIn('janeway')}>Sign in</StyledButton>
+  return <StyledLink onClick={() => signIn('janeway')}>Sign in</StyledLink>
 }
 
 export default function AuthTesting() {
