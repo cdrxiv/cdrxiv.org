460801fef88bca437a12db67c5c8cfc3a30140dd
Kata Martin <katasm@gmail.com>
2024-07-22 13:56:34 -0400

    Att fetchWithToken() util

--- app/api/user-preprints/route.tsx
+++ app/api/user-preprints/route.tsx
@@ -1,33 +1,10 @@
-import { getToken } from 'next-auth/jwt'
 import type { NextRequest } from 'next/server'
 
+import { fetchWithToken } from '../utils'
+
 export async function GET(request: NextRequest) {
-  const token = await getToken({
-    req: request,
-    secret: process.env.NEXTAUTH_SECRET,
-  })
-
-  if (!token) {
-    return new Response('Not authenticated', {
-      status: 403,
-    })
-  }
-
-  const res = await fetch(
+  return fetchWithToken(
+    request,
     'https://carbonplan.endurance.janeway.systems/carbonplan/api/user_preprints/',
-    {
-      headers: {
-        Authorization: `Bearer ${token.accessToken}`,
-      },
-    },
   )
-  const data = await res.json()
-
-  if (!data.results) {
-    return new Response(data.detail ?? 'Request not completed', {
-      status: 403,
-    })
-  }
-
-  return Response.json(data)
 }
