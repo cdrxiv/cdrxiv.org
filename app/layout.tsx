15d289a51f7dc0d886216f9e1146e47db79a13e6
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-07-29 15:04:56 -0500

    add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <link
+          rel='preload'
+          href='https://fonts.carbonplan.org/quadrant/QuadrantText-Regular.woff2'
+          as='font'
+          type='font/woff2'
+          crossOrigin='anonymous'
+        />
+        <link
+          rel='preload'
+          href='https://fonts.carbonplan.org/gt_pressura_mono/GT-Pressura-Mono-Regular.woff2'
+          as='font'
+          type='font/woff2'
+          crossOrigin='anonymous'
+        />
+      </head>
       <body>
         <ThemeWrapper>
           <main>
--- app/theme-wrapper.tsx
+++ app/theme-wrapper.tsx
@@ -2,9 +2,35 @@
 
 import { ThemeUIProvider } from 'theme-ui'
 import { theme } from '../theme/theme'
+import { css, Global } from '@emotion/react'
 
 const ThemeWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeUIProvider theme={theme}>{children}</ThemeUIProvider>
+  return (
+    <ThemeUIProvider theme={theme}>
+      <Global
+        styles={css`
+          @font-face {
+            font-family: 'Quadrant Text Regular';
+            src: url('https://fonts.carbonplan.org/quadrant/QuadrantText-Regular.woff2')
+              format('woff2');
+            font-weight: 400;
+            font-style: normal;
+            font-display: swap;
+          }
+
+          @font-face {
+            font-family: 'GT Pressura Mono Regular';
+            src: url('https://fonts.carbonplan.org/gt_pressura_mono/GT-Pressura-Mono-Regular.woff2')
+              format('woff2');
+            font-weight: 400;
+            font-style: normal;
+            font-display: swap;
+          }
+        `}
+      />
+      {children}
+    </ThemeUIProvider>
+  )
 }
 
 export default ThemeWrapper
