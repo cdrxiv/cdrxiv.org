6155fab3622b5dcc3fc629a4159b817737d727fb
Kata Martin <katasm@gmail.com>
2024-07-31 14:06:18 -0400

    Move user info out of sidebar

--- app/submit/layout.tsx
+++ app/submit/layout.tsx
@@ -1,46 +1,12 @@
 'use client'
 
-import { SessionProvider, useSession, signIn, signOut } from 'next-auth/react'
+import { SessionProvider } from 'next-auth/react'
 import { Box, Flex } from 'theme-ui'
-import { useState } from 'react'
 import { usePathname } from 'next/navigation'
 
 import StyledLink from '../../components/link'
 import PaneledPage from '../../components/layouts/paneled-page'
 import NavLink from '../../components/nav-link'
-
-const UserInfo = () => {
-  const [expanded, setExpanded] = useState(false)
-  const { data: session, status } = useSession()
-
-  if (status === 'authenticated' && session) {
-    return (
-      <Box sx={{ position: 'relative' }}>
-        <StyledLink onClick={() => setExpanded(!expanded)}>
-          Signed in as {session?.user?.email ?? 'Unknown'}{' '}
-          <Box
-            sx={{
-              display: 'inline-block',
-              transform: expanded ? 'rotate(90deg)' : 'rotate(0deg)',
-              transition: 'transform 0.2s',
-            }}
-          >
-            {'>>'}
-          </Box>
-        </StyledLink>
-        {expanded && (
-          <Box sx={{ position: 'absolute', top: '28px' }}>
-            <StyledLink onClick={() => signOut({ callbackUrl: '/' })}>
-              Sign out
-            </StyledLink>
-          </Box>
-        )}
-      </Box>
-    )
-  }
-
-  return <StyledLink onClick={() => signIn('janeway')}>Sign in</StyledLink>
-}
 
 const PATHS = [
   { label: 'Submission Overview', href: '/submit/overview' },
@@ -57,8 +23,6 @@ const Submit: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
   return (
     <SessionProvider>
-      <UserInfo />
-
       <PaneledPage
         title={active.label}
         corner={`Step ${index + 1} / ${PATHS.length}`}
--- app/submit/overview/page.tsx
+++ app/submit/overview/page.tsx
@@ -1,7 +1,35 @@
+'use client'
+
+import { useSession, signIn, signOut } from 'next-auth/react'
+import { Box } from 'theme-ui'
+
+import StyledLink from '../../../components/link'
+import Expander from '../../../components/expander'
+
+const UserInfo = () => {
+  const { data: session, status } = useSession()
+
+  if (status === 'authenticated' && session) {
+    return (
+      <Box sx={{ position: 'relative' }}>
+        <Expander label={`Signed in as ${session?.user?.email ?? 'Unknown'}`}>
+          <Box sx={{ position: 'absolute', top: '28px' }}>
+            <StyledLink onClick={() => signOut({ callbackUrl: '/' })}>
+              Sign out
+            </StyledLink>
+          </Box>
+        </Expander>
+      </Box>
+    )
+  }
+
+  return <StyledLink onClick={() => signIn('janeway')}>Sign in</StyledLink>
+}
+
 const SubmissionOverview = () => {
   return (
     <div>
-      <div>Some other stuff</div>
+      <UserInfo />
     </div>
   )
 }
