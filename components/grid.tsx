92250638417d830033174b50217e3a8449ab97d5
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-07-31 11:27:19 -0500

    use row instead of grid

--- components/grid.tsx
+++ components/grid.tsx
@@ -1,18 +1,14 @@
 import React from 'react'
-import { Grid } from 'theme-ui'
 import Card from './card'
+import Row from './row'
 import type { Preprints } from '../types/preprint'
-
 interface GridViewProps {
   preprints: Preprints
 }
 
 const GridView: React.FC<GridViewProps> = ({ preprints }) => {
   return (
-    <Grid
-      gap={[6, 6, 6, 8]}
-      columns={['1fr', '1fr 1fr', '1fr 1fr 1fr', 'repeat(4, 1fr)']}
-    >
+    <Row columns={[1, 2, 3, 4]} gap={[5, 6, 6, 8]}>
       {preprints.map(({ title, authors, date_published }) => (
         <Card
           key={title}
@@ -22,7 +18,7 @@ const GridView: React.FC<GridViewProps> = ({ preprints }) => {
           date={date_published ? new Date(date_published) : null}
         />
       ))}
-    </Grid>
+    </Row>
   )
 }
 
