eca5f3f9223c2bbd7335df11629f84292e4507fb
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-07-31 11:45:08 -0500

    fully responsive list

--- components/list.tsx
+++ components/list.tsx
@@ -29,68 +29,57 @@ const ListView: React.FC<ListViewProps> = ({ preprints }) => {
             },
           }}
         >
-          {/* Desktop view */}
-          <Box sx={{ display: ['none', 'none', 'inherit'] }}>
-            <Row columns={12} sx={{ alignItems: 'center' }}>
-              <Column start={1} width={4}>
-                <Box
-                  id='title'
-                  sx={{ variant: 'text.body', alignSelf: 'center' }}
-                >
-                  {preprint.title}
-                </Box>
-              </Column>
-              <Column start={5} width={2}>
-                <Box sx={{ variant: 'text.mono', alignSelf: 'baseline' }}>
-                  {authorList(preprint.authors, true)}
-                </Box>
-              </Column>
-              <Column start={7} width={2}>
+          <Row columns={12} sx={{ alignItems: 'center', flexWrap: 'wrap' }}>
+            <Column start={[1, 1, 1, 1]} width={[12, 12, 4, 4]}>
+              <Box
+                id='title'
+                sx={{
+                  variant: 'text.body',
+                  color: ['blue', 'blue', 'inherit', 'inherit'],
+                  mb: [2, 2, 0, 0],
+                }}
+              >
+                {preprint.title}
+              </Box>
+            </Column>
+            <Column start={[1, 1, 5, 5]} width={[12, 12, 2, 2]}>
+              <Box
+                sx={{
+                  variant: 'text.mono',
+                  mb: [2, 2, -1, 0], // baseline adjustment at [2]
+                }}
+              >
+                {authorList(preprint.authors, true)}
+              </Box>
+            </Column>
+            <Column start={[1, 1, 7, 7]} width={[12, 12, 2, 2]}>
+              <Box sx={{ mb: [6, 6, 0, 0] }}>
                 <Badge color={'pink'}>Article</Badge>
-              </Column>
-              <Column start={9} width={2}>
-                <Box sx={{ variant: 'text.monoCaps', alignSelf: 'baseline' }}>
-                  {preprint.date_published
-                    ? formatDate(new Date(preprint.date_published))
-                    : null}
-                </Box>
-              </Column>
-              <Column start={11} width={2}>
-                <Box sx={{ variant: 'text.mono', alignSelf: 'center' }}>
-                  {preprint.subject.map((sub, i) => (
-                    <React.Fragment key={sub.name}>
-                      {sub.name}
-                      {i !== preprint.subject.length - 1 ? ', ' : null}
-                    </React.Fragment>
-                  ))}
-                </Box>
-              </Column>
-            </Row>
-          </Box>
-
-          {/* Mobile view */}
-          <Box sx={{ display: ['inherit', 'inherit', 'none'] }}>
-            <Box sx={{ variant: 'text.body', color: 'blue' }}>
-              {preprint.title}
-            </Box>
-            <Box sx={{ variant: 'text.mono', my: 2 }}>
-              {authorList(preprint.authors, true)}
-            </Box>
-            <Badge color={'pink'}>Article</Badge>
-            <Box sx={{ variant: 'text.monoCaps', mt: 6 }}>
-              {preprint.date_published
-                ? formatDate(new Date(preprint.date_published))
-                : null}
-            </Box>
-            <Box sx={{ variant: 'text.mono', mt: 1 }}>
-              {preprint.subject.map((sub, i) => (
-                <React.Fragment key={sub.name}>
-                  {sub.name}
-                  {i !== preprint.subject.length - 1 ? ', ' : null}
-                </React.Fragment>
-              ))}
-            </Box>
-          </Box>
+              </Box>
+            </Column>
+            <Column start={[1, 1, 9, 9]} width={[12, 12, 2, 2]}>
+              <Box
+                sx={{
+                  variant: 'text.monoCaps',
+                  mb: [1, 1, 0, 0],
+                }}
+              >
+                {preprint.date_published
+                  ? formatDate(new Date(preprint.date_published))
+                  : null}
+              </Box>
+            </Column>
+            <Column start={[1, 1, 11, 11]} width={[12, 12, 2, 2]}>
+              <Box sx={{ variant: 'text.mono' }}>
+                {preprint.subject.map((sub, i) => (
+                  <React.Fragment key={sub.name}>
+                    {sub.name}
+                    {i !== preprint.subject.length - 1 ? ', ' : null}
+                  </React.Fragment>
+                ))}
+              </Box>
+            </Column>
+          </Row>
         </Box>
       ))}
     </>
