03d4cc50431150eeee8dc58f1e6b558e29a959b4
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-07-31 15:47:13 -0500

    fix route

--- app/api/subjects/route.tsx
+++ app/api/subjects/route.tsx
@@ -1,8 +1,6 @@
 import type { NextRequest } from 'next/server'
-import { fetchWithToken } from '../utils'
-import { TEST_SUBJECTS } from '../placeholder-data'
-import { getTopics } from '../utils'
+import { getSubjects } from '../utils'
 
 export async function GET(request: NextRequest) {
-  return getTopics(request)
+  return getSubjects(request)
 }
