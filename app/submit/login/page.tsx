793435e68342895502b1c38ac8d419ac6db5b721
Kata Martin <katasm@gmail.com>
2024-07-31 16:42:57 -0400

    Improve login UI

--- app/submit/login/page.tsx
+++ app/submit/login/page.tsx
@@ -5,31 +5,46 @@ import { Box, Flex } from 'theme-ui'
 
 import StyledLink from '../../../components/link'
 import Expander from '../../../components/expander'
-
-const UserInfo = () => {
-  const { data: session, status } = useSession()
-
-  if (status === 'authenticated' && session) {
-    return (
-      <Box sx={{ position: 'relative' }}>
-        <Expander label={`Signed in as ${session?.user?.email ?? 'Unknown'}`}>
-          <Box sx={{ top: '28px' }}>
-            <StyledLink onClick={() => signOut({ callbackUrl: '/' })}>
-              Sign out
-            </StyledLink>
-          </Box>
-        </Expander>
-      </Box>
-    )
-  }
-
-  return <StyledLink onClick={() => signIn('janeway')}>Sign in</StyledLink>
-}
+import Field from '../../../components/field'
+import StyledButton from '../../../components/button'
+import Row from '../../../components/row'
+import Column from '../../../components/column'
 
 const SubmissionLogin = () => {
+  const { data: session, status } = useSession()
+
   return (
     <Flex sx={{ flexDirection: 'column', gap: 7 }}>
-      <UserInfo />
+      {status === 'authenticated' && session && (
+        <Box sx={{ position: 'relative' }}>
+          <Expander label={`Signed in as ${session?.user?.email ?? 'Unknown'}`}>
+            <Box sx={{ top: '28px' }}>
+              <StyledLink
+                onClick={() => signOut({ callbackUrl: '/' })}
+                sx={{ position: 'absolute', variant: 'text.monoCaps' }}
+              >
+                Sign out
+              </StyledLink>
+            </Box>
+          </Expander>
+        </Box>
+      )}
+
+      {status === 'unauthenticated' && (
+        <Field
+          label=''
+          id='signin'
+          description='CDRXIV uses Janeway for authentication. Use your Janeway account credentials to log in and get started with your submission.'
+        >
+          <Row columns={6}>
+            <Column start={1} width={[3, 4, 3, 3]}>
+              <StyledButton onClick={() => signIn('janeway')}>
+                Log in with Janeway
+              </StyledButton>
+            </Column>
+          </Row>
+        </Field>
+      )}
     </Flex>
   )
 }
