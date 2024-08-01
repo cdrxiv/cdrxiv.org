3da87bfdaa859c9a2b28513f00039b72072f7fac
Kata Martin <katasm@gmail.com>
2024-08-01 15:10:31 -0400

    Clean up middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,8 @@
 import { withAuth } from 'next-auth/middleware'
 
 export const middleware = withAuth({
-  // Matches the pages config in `[...nextauth]`
   pages: {
     signIn: '/submit/login',
-    error: '/error',
   },
 })
 
