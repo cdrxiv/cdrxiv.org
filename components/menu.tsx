9a67a9294e7b76b5a2c2b18a9048d78b99106ea1
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-08-06 15:17:09 -0500

    clean up

--- components/menu.tsx
+++ components/menu.tsx
@@ -50,8 +50,7 @@ const Menu: React.FC<MenuProps> = ({ setMenuOpen, children, sx }) => {
             color: 'blue',
             bg: 'transparent',
             cursor: 'pointer',
-            // zIndex: 1,
-            padding: 0,
+            p: 0,
             mt: -6,
             alignSelf: 'flex-end',
           }}
