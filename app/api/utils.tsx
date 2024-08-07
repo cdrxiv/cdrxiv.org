330e0a5e636b6fd9553490aaec106b27e5d52444
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-08-07 15:20:37 -0500

    add cache time to preprints

--- app/api/utils.tsx
+++ app/api/utils.tsx
@@ -50,7 +50,7 @@ export const getPreprints = async (subject?: string) => {
     const queryString = `subject=${subject}`
     url = `${url}?${queryString}`
   }
-  const res = await fetch(url)
+  const res = await fetch(url, { next: { revalidate: 3600 } })
   if (res.status === 200) {
     return await res.json()
   }
