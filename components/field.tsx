4a92b3532c8b27544443109407de79b61bfabcd6
Kata Martin <katasm@gmail.com>
2024-08-07 13:01:59 -0400

    Tweak error state

--- components/field.tsx
+++ components/field.tsx
@@ -16,7 +16,10 @@ const Field: React.FC<Props> = ({
 }) => {
   return (
     <Flex sx={{ flexDirection: 'column', gap: 2 }}>
-      <Label htmlFor={id}>{label}</Label>
+      <Label htmlFor={id}>
+        {label}
+        {error && <Box sx={{ color: 'red' }}>&nbsp;(!)</Box>}{' '}
+      </Label>
       {children}
       {description && <Box sx={{ variant: 'text.mono' }}>{description}</Box>}
       {error && <Box sx={{ variant: 'text.mono', color: 'red' }}>{error}</Box>}
