924a4bb711b97e41f6bec8473fc6b33ee86996ba
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-08-08 09:57:51 -0500

    responsive bg colors

--- components/layouts/page-card.tsx
+++ components/layouts/page-card.tsx
@@ -4,48 +4,53 @@ import React from 'react'
 import { Box } from 'theme-ui'
 import Header from '../header'
 import Guide from '../guide'
+import useBackgroundColors from '../../hooks/useBackgroundColors'
 
 const margin = [2, 2, 3, 3]
 
 const PageCard = ({ children }: { children: React.ReactNode }) => {
+  const { cardBackground, overallBackground } = useBackgroundColors()
+
   return (
-    <Box
-      sx={{
-        m: margin,
-        height: (theme) =>
-          margin.map(
-            (space) =>
-              `calc(100vh - 2 * ${theme.space ? theme.space[space] : 0}px)`,
-          ),
-        width: (theme) =>
-          margin.map(
-            (space) =>
-              `calc(100vw - 2 * ${theme.space ? theme.space[space] : 0}px)`,
-          ),
-        position: 'fixed',
-      }}
-    >
+    <Box sx={{ bg: overallBackground, width: '100vw', height: '100vh' }}>
       <Box
         sx={{
-          height: '100%',
-          overflow: 'auto',
-          bg: 'background',
-          borderLeft: '1px solid',
-          borderBottom: '1px solid',
-          borderRight: '1px solid',
-          borderColor: 'text',
+          m: margin,
+          height: (theme) =>
+            margin.map(
+              (space) =>
+                `calc(100vh - 2 * ${theme.space ? theme.space[space] : 0}px)`,
+            ),
+          width: (theme) =>
+            margin.map(
+              (space) =>
+                `calc(100vw - 2 * ${theme.space ? theme.space[space] : 0}px)`,
+            ),
+          position: 'fixed',
         }}
       >
-        <Header />
         <Box
           sx={{
-            mt: 100,
-            px: ['18px', '36px', '36px', '52px'],
+            height: '100%',
+            overflow: 'auto',
+            bg: cardBackground,
+            borderLeft: '1px solid',
+            borderBottom: '1px solid',
+            borderRight: '1px solid',
+            borderColor: 'text',
           }}
         >
-          <Box sx={{ contain: 'layout' }}>
-            <Guide />
-            {children}
+          <Header />
+          <Box
+            sx={{
+              mt: 100,
+              px: ['18px', '36px', '36px', '52px'],
+            }}
+          >
+            <Box sx={{ contain: 'layout' }}>
+              <Guide />
+              {children}
+            </Box>
           </Box>
         </Box>
       </Box>
---
+++ hooks/useBackgroundColors.ts
@@ -0,0 +1,23 @@
+import { usePathname } from 'next/navigation'
+
+type ColorsObject = {
+  cardBackground: string[]
+  overallBackground: string[]
+}
+
+const useBackgroundColors = (): ColorsObject => {
+  const pathname = usePathname()
+  // TODO: expand this to include preprint viewing paths
+  if (pathname.includes('submit')) {
+    return {
+      cardBackground: ['primary', 'primary', 'background', 'background'],
+      overallBackground: ['background', 'background', 'primary', 'primary'],
+    }
+  }
+  return {
+    cardBackground: ['background'],
+    overallBackground: ['primary'],
+  }
+}
+
+export default useBackgroundColors
