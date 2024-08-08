cccf5ca9673b70b9ddd099e03fd6061bff4c447a
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-08-08 12:50:08 -0500

    update middleware to catch submit redirect earlier

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,20 @@
+import { NextResponse } from 'next/server'
 import { withAuth } from 'next-auth/middleware'
+import type { NextRequestWithAuth } from 'next-auth/middleware'
 
-export const middleware = withAuth({
-  pages: {
-    signIn: '/submit/login',
+export const middleware = withAuth(
+  function middleware(request: NextRequestWithAuth) {
+    if (request.nextUrl.pathname === '/submit') {
+      return NextResponse.redirect(new URL('/submit/overview', request.url))
+    }
+    return NextResponse.next()
   },
-})
+  {
+    pages: {
+      signIn: '/submit/login',
+    },
+  },
+)
 
 export const config = {
   matcher: [
