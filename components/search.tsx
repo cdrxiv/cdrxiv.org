9e9d501671b62117d8cbc2272475a1025750a7f2
Kata Martin <katasm@gmail.com>
2024-08-09 13:33:38 -0400

    Extend search for use on authors page

--- components/search.tsx
+++ components/search.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Box, Input, ThemeUIStyleObject } from 'theme-ui'
+import { Box, Input, InputProps, ThemeUIStyleObject } from 'theme-ui'
 
-interface SearchProps {
+interface SearchProps extends InputProps {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
   placeholder?: string
   arrows?: boolean
   sx?: ThemeUIStyleObject
+  inverted?: boolean
 }
 
 const Search: React.FC<SearchProps> = ({
   onChange,
   placeholder,
+  inverted,
   arrows = true,
   sx = {},
 }) => {
@@ -21,9 +23,15 @@ const Search: React.FC<SearchProps> = ({
         onChange={onChange}
         sx={{
           variant: 'text.monoCaps',
-          boxShadow: (theme) => `1px 1px 1px 1px ${theme?.colors?.text} inset, 
-                -1px -1px 1px 1px ${theme?.colors?.muted} inset`,
-          background: 'primary',
+          ...(inverted
+            ? {
+                boxShadow: (
+                  theme,
+                ) => `1px 1px 1px 1px ${theme?.colors?.text} inset,
+          -1px -1px 1px 1px ${theme?.colors?.muted} inset`,
+                background: 'primary',
+              }
+            : {}),
           pr: arrows ? 5 : 3,
           '::placeholder': { color: 'text' },
           ...sx,
