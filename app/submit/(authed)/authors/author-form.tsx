11aceba3f12fb62f4438d171d30ef59c44a5e8ae
Kata Martin <katasm@gmail.com>
2024-08-12 10:55:15 -0400

    Tweak spacing

--- app/submit/(authed)/authors/author-form.tsx
+++ app/submit/(authed)/authors/author-form.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box, Flex, Input } from 'theme-ui'
+import { Box, Input } from 'theme-ui'
 
 import Field from '../../../../components/field'
 import { usePreprint } from '../preprint-context'
@@ -78,72 +78,70 @@ const AuthorForm = () => {
 
   return (
     <>
-      <Flex sx={{ flexDirection: 'column', gap: 7 }}>
-        {submitError && <Box sx={{ color: 'red' }}>{submitError}</Box>}
-        <Box>
-          <Row columns={[6, 6, 9, 9]} sx={{ mt: 3 }}>
-            <Column start={1} width={[6, 2, 3, 3]}>
-              <Field
-                label='First name*'
+      {submitError && <Box sx={{ color: 'red' }}>{submitError}</Box>}
+      <Box>
+        <Row columns={[6, 6, 9, 9]} sx={{ mt: 3 }}>
+          <Column start={1} width={[6, 2, 3, 3]}>
+            <Field
+              label='First name*'
+              id='first_name'
+              error={errors.first_name}
+            >
+              <Input
+                value={data.first_name}
+                onChange={(e) => setters.first_name(e.target.value)}
                 id='first_name'
-                error={errors.first_name}
-              >
-                <Input
-                  value={data.first_name}
-                  onChange={(e) => setters.first_name(e.target.value)}
-                  id='first_name'
-                />
-              </Field>
-            </Column>
-            <Column start={[1, 3, 4, 4]} width={[6, 2, 3, 3]}>
-              <Field
-                label='Middle name'
+              />
+            </Field>
+          </Column>
+          <Column start={[1, 3, 4, 4]} width={[6, 2, 3, 3]}>
+            <Field
+              label='Middle name'
+              id='middle_name'
+              error={errors.middle_name}
+            >
+              <Input
+                value={data.middle_name}
+                onChange={(e) => setters.middle_name(e.target.value)}
                 id='middle_name'
-                error={errors.middle_name}
-              >
-                <Input
-                  value={data.middle_name}
-                  onChange={(e) => setters.middle_name(e.target.value)}
-                  id='middle_name'
-                />
-              </Field>
-            </Column>
-            <Column start={[1, 5, 7, 7]} width={[6, 2, 3, 3]}>
-              <Field label='Last name*' id='last_name' error={errors.last_name}>
-                <Input
-                  value={data.last_name}
-                  onChange={(e) => setters.last_name(e.target.value)}
-                  id='last_name'
-                />
-              </Field>
-            </Column>
-          </Row>
-          <Row columns={[6, 6, 8, 8]} sx={{ mt: [0, 3, 3, 3] }}>
-            <Column start={1} width={[6, 3, 4, 4]}>
-              <Field label='Email*' id='email' error={errors.email}>
-                <Input
-                  value={data.email}
-                  onChange={(e) => setters.email(e.target.value)}
-                  id='email'
-                />
-              </Field>
-            </Column>
-            <Column start={[1, 4, 5, 5]} width={[6, 3, 4, 4]}>
-              <Field
-                label='Affiliation'
+              />
+            </Field>
+          </Column>
+          <Column start={[1, 5, 7, 7]} width={[6, 2, 3, 3]}>
+            <Field label='Last name*' id='last_name' error={errors.last_name}>
+              <Input
+                value={data.last_name}
+                onChange={(e) => setters.last_name(e.target.value)}
+                id='last_name'
+              />
+            </Field>
+          </Column>
+        </Row>
+        <Row columns={[6, 6, 8, 8]} sx={{ mt: [0, 3, 3, 3] }}>
+          <Column start={1} width={[6, 3, 4, 4]}>
+            <Field label='Email*' id='email' error={errors.email}>
+              <Input
+                value={data.email}
+                onChange={(e) => setters.email(e.target.value)}
+                id='email'
+              />
+            </Field>
+          </Column>
+          <Column start={[1, 4, 5, 5]} width={[6, 3, 4, 4]}>
+            <Field
+              label='Affiliation'
+              id='institution'
+              error={errors.institution}
+            >
+              <Input
+                value={data.institution}
+                onChange={(e) => setters.institution(e.target.value)}
                 id='institution'
-                error={errors.institution}
-              >
-                <Input
-                  value={data.institution}
-                  onChange={(e) => setters.institution(e.target.value)}
-                  id='institution'
-                />
-              </Field>
-            </Column>
-          </Row>
-        </Box>
-      </Flex>
+              />
+            </Field>
+          </Column>
+        </Row>
+      </Box>
       <StyledButton onClick={onSubmit} sx={{ width: 'fit-content' }}>
         Add author
       </StyledButton>
