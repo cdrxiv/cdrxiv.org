4de1ce10328bbac696d5581755b3e998aa073504
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-08-13 10:21:48 -0500

    resizing

--- app/preprint/[id]/pdf-viewer.tsx
+++ app/preprint/[id]/pdf-viewer.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import React, { useCallback, useState, useEffect, useRef } from 'react'
 import { Document, Outline, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/Page/TextLayer.css'
 import 'react-pdf/dist/Page/AnnotationLayer.css'
@@ -16,6 +16,33 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/b
 const PdfViewer = ({ preprint }: { preprint: Preprint }) => {
   const pdfProxyUrl = `/api/pdf/?url=${encodeURIComponent(preprint.versions[0].public_download_url)}`
   const [numPages, setNumPages] = useState(0)
+  const [containerWidth, setContainerWidth] = useState<number>()
+  const containerRef = useRef<HTMLDivElement>(null)
+  const resizeTimeoutRef = useRef<NodeJS.Timeout | null>(null)
+
+  const onResize = useCallback<ResizeObserverCallback>((entries) => {
+    const [entry] = entries
+    if (entry) {
+      if (resizeTimeoutRef.current) {
+        clearTimeout(resizeTimeoutRef.current)
+      }
+      resizeTimeoutRef.current = setTimeout(() => {
+        setContainerWidth(entry.contentRect.width)
+      }, 100)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!containerRef.current) return
+    const resizeObserver = new ResizeObserver(onResize)
+    resizeObserver.observe(containerRef.current)
+    return () => {
+      if (resizeTimeoutRef.current) {
+        clearTimeout(resizeTimeoutRef.current)
+      }
+      resizeObserver.disconnect()
+    }
+  }, [onResize])
 
   const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
     setNumPages(numPages)
@@ -35,17 +62,19 @@ const PdfViewer = ({ preprint }: { preprint: Preprint }) => {
       <Box sx={{ variant: 'text.mono', mt: 3, mb: 4 }}>
         {authorList(preprint.authors)}
       </Box>
-      <Document file={pdfProxyUrl} onLoadSuccess={onDocumentLoadSuccess}>
-        <Outline />
-        {numPages > 0 &&
-          Array.from(new Array(numPages), (_, index) => (
-            <Page
-              key={`page_${index + 1}`}
-              pageNumber={index + 1}
-              loading={''}
-            />
-          ))}
-      </Document>
+      <div ref={containerRef} style={{ width: '100%' }}>
+        <Document file={pdfProxyUrl} onLoadSuccess={onDocumentLoadSuccess}>
+          {numPages > 0 &&
+            Array.from(new Array(numPages), (_, index) => (
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                width={containerWidth}
+                loading={''}
+              />
+            ))}
+        </Document>
+      </div>
     </PaneledPage>
   )
 }
