6f4e6c37dbdd29b66b61e43596b73b701295d3f0
Kata Martin <katasm@gmail.com>
2024-08-14 15:30:27 -0400

    Move getAdditionalField to shared helper

---
+++ utils/data.ts
@@ -0,0 +1,20 @@
+import { Preprint } from '../types/preprint'
+
+export const getAdditionalField = (
+  preprint: Preprint | null,
+  fieldName: string,
+) => {
+  if (!preprint) {
+    return null
+  }
+
+  const additionalField = preprint.additional_field_answers.find(
+    (field) => field.field?.name === fieldName,
+  )
+
+  if (!additionalField) {
+    return null
+  }
+
+  return additionalField.answer
+}
