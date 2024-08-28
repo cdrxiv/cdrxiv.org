15eaba7345fc0e8f6ed0d21a618888144389478d
Kata Martin <katasm@gmail.com>
2024-08-28 12:00:01 -0700

    Temp fix

--- app/(userSection)/submissions/(index)/page.tsx
+++ app/(userSection)/submissions/(index)/page.tsx
@@ -3,36 +3,52 @@ import React from 'react'
 import { redirect } from 'next/navigation'
 
 import SubmissionsView from '../submissions-view'
+import { Preprint } from '../../../../types/preprint'
 import { fetchWithToken } from '../../../api/utils'
 
 const SubmissionOverview = async () => {
-  const responses = await Promise.all([
-    fetchWithToken(
-      headers(),
-      'https://carbonplan.endurance.janeway.systems/carbonplan/api/user_preprints/?stage=preprint_published',
-    ),
-    fetchWithToken(
-      headers(),
-      'https://carbonplan.endurance.janeway.systems/carbonplan/api/user_preprints/?stage=preprint_review',
-    ),
-    fetchWithToken(
-      headers(),
-      'https://carbonplan.endurance.janeway.systems/carbonplan/api/user_preprints/?stage=preprint_rejected',
-    ),
-  ])
+  // const responses = await Promise.all([
+  //   fetchWithToken(
+  //     headers(),
+  //     'https://carbonplan.endurance.janeway.systems/carbonplan/api/user_preprints/?stage=preprint_published',
+  //   ),
+  //   fetchWithToken(
+  //     headers(),
+  //     'https://carbonplan.endurance.janeway.systems/carbonplan/api/user_preprints/?stage=preprint_review',
+  //   ),
+  //   fetchWithToken(
+  //     headers(),
+  //     'https://carbonplan.endurance.janeway.systems/carbonplan/api/user_preprints/?stage=preprint_rejected',
+  //   ),
+  // ])
 
-  if (responses.some((res) => res.status !== 200)) {
-    redirect('/login?signOut=true')
-  }
-  const [published, review, rejected] = await Promise.all(
-    responses.map((res) => res.json()),
+  // if (responses.some((res) => res.status !== 200)) {
+  //   redirect('/login?signOut=true')
+  // }
+  // const [published, review, rejected] = await Promise.all(
+  //   responses.map((res) => res.json()),
+  // )
+
+  const response = await fetchWithToken(
+    headers(),
+    'https://carbonplan.endurance.janeway.systems/carbonplan/api/user_preprints/',
   )
+  if (response.status !== 200) {
+  }
+
+  // TODO: remove manual filtering when stage querying is restored
+  const results = await response.json()
+  const [published, review, rejected] = [
+    'preprint_published',
+    'preprint_review',
+    'preprint_rejected',
+  ].map((stage) => results.results.filter((p: Preprint) => p.stage === stage))
 
   return (
     <SubmissionsView
-      published={published.results}
-      review={review.results}
-      rejected={rejected.results}
+      published={published}
+      review={review}
+      rejected={rejected}
     />
   )
 }
