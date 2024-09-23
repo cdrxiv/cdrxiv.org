7ee83d91b43687ea6f07ef03e9b06214b219bf3a
Kata Martin <katasm@gmail.com>
2024-09-23 11:00:23 -0400

    Remove select all option

--- app/submit/(authed)/info/subjects.tsx
+++ app/submit/(authed)/info/subjects.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Flex, Label } from 'theme-ui'
-import { Checkbox, Link } from '../../../../components'
+import { Checkbox } from '../../../../components'
 import { useSubjects } from '../../../subjects-context'
 
 const Subjects: React.FC<{
@@ -10,20 +10,8 @@ const Subjects: React.FC<{
 }> = ({ value, onChange }) => {
   const subjects = useSubjects()
 
-  const hasSelectedAll = value.length === subjects.length
   return (
     <Flex sx={{ flexDirection: 'column' }}>
-      <Link
-        onClick={() =>
-          hasSelectedAll
-            ? onChange([])
-            : onChange(subjects.map(({ name }) => name))
-        }
-        sx={{ variant: 'text.mono', mb: 2 }}
-      >
-        {hasSelectedAll ? 'Select none' : 'Select all'}
-      </Link>
-
       {subjects.map(({ name }) => (
         <Label
           key={name}
