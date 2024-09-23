393bb7729c7757b11e5291fab69e5f74e85d2e47
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-09-23 14:31:30 -0500

    use next's loading pattern

--- app/preprint/[id]/loading.tsx
+++ app/preprint/[id]/loading.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import { Flex } from 'theme-ui'
+import { Loading } from '../../../components'
 import PaneledPage from '../../../components/layouts/paneled-page'
 
-export default function NotFound() {
+export default function PreprintLoading() {
   return (
     <PaneledPage sidebar={<></>}>
       <Flex sx={{ justifyContent: 'center', alignItems: 'center', my: 150 }}>
-        Preprint not found
+        <Loading />
       </Flex>
     </PaneledPage>
   )
