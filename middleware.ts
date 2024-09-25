73543eefdb8cc4f1c0888e0675ef4ea844cf25e7
Kata Martin <katasm@gmail.com>
2024-09-25 17:31:29 -0400

    Set up middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,42 @@
-import { withAuth } from 'next-auth/middleware'
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware'
+import { isFullSiteEnabled } from './utils/flags'
+import { NextFetchEvent, NextResponse } from 'next/server'
 
-export const middleware = withAuth({
+const AUTHED_ROUTES = [
+  '/submissions',
+  '/preview',
+  '/submit/overview',
+  '/submit/info',
+  '/submit/authors',
+  '/submit/confirm',
+]
+const withAuthMiddleware = withAuth({
   pages: {
     signIn: '/account',
   },
 })
+export const middleware = (
+  request: NextRequestWithAuth,
+  event: NextFetchEvent,
+) => {
+  if (isFullSiteEnabled()) {
+    if (AUTHED_ROUTES.includes(request.nextUrl.pathname)) {
+      return withAuthMiddleware(request, event)
+    } else {
+      NextResponse.next()
+      return
+    }
+  }
+
+  if (request.nextUrl.pathname !== '/') {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  NextResponse.next()
+}
 
 export const config = {
   matcher: [
-    '/submissions',
-    '/preview',
-    '/submit/overview',
-    '/submit/info',
-    '/submit/authors',
-    '/submit/confirm',
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
   ],
 }
