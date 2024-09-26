407847522cbc77f365e97bb1c06662dea72d29dc
Kata Martin <katasm@gmail.com>
2024-09-26 10:06:27 -0400

    Change submission step title

--- app/submit/constants.ts
+++ app/submit/constants.ts
@@ -1,5 +1,5 @@
 export const PATHS = [
-  { title: 'Overview', href: '/submit/overview' },
+  { title: 'Submission', href: '/submit/overview' },
   { title: 'Information', href: '/submit/info' },
   { title: 'Authors', href: '/submit/authors' },
   { title: 'Confirmation', href: '/submit/confirm' },
