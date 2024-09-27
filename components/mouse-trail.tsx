72623fcb954556de9cc39a3ffe3031e8a094ba1c
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-09-27 14:10:17 -0700

    add a global override style of cursor when trails are active

--- components/mouse-trail.tsx
+++ components/mouse-trail.tsx
@@ -2,7 +2,6 @@
 
 import { AnimatePresence, motion } from 'framer-motion'
 import { useCallback, useEffect, useState } from 'react'
-
 import { v4 as uuidv4 } from 'uuid'
 
 interface CursorPosition {
@@ -23,7 +22,7 @@ const MouseTrail = ({ isActive }: CursorTrailProps) => {
       if (isActive) {
         setCursorTrail((previousTrail) => [
           { x: event.clientX, y: event.clientY, id: uuidv4() },
-          ...previousTrail.slice(0, 10),
+          ...previousTrail.slice(0, 14),
         ])
       }
     },
@@ -46,53 +45,62 @@ const MouseTrail = ({ isActive }: CursorTrailProps) => {
   }, [isActive])
 
   return (
-    <div
-      style={{
-        position: 'fixed',
-        inset: 0,
-        pointerEvents: 'none',
-        zIndex: 9999,
-      }}
-    >
-      <AnimatePresence>
-        {isActive &&
-          cursorTrail.map((cursor, index) => (
-            <motion.div
-              key={cursor.id}
-              initial={{ opacity: 0.8, scale: 1 }}
-              animate={{ opacity: 0, scale: 0.8 }}
-              exit={{ opacity: 0, scale: 0.5 }}
-              transition={{ duration: 0.5, ease: 'easeOut' }}
-              style={{
-                position: 'absolute',
-                left: cursor.x - 10,
-                top: cursor.y + 5,
-              }}
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                height='32'
-                viewBox='0 0 32 32'
-                width='32'
+    <>
+      {isActive && (
+        <style jsx global>{`
+          * {
+            cursor: none !important;
+          }
+        `}</style>
+      )}
+      <div
+        style={{
+          position: 'fixed',
+          inset: 0,
+          pointerEvents: 'none',
+          zIndex: 9999,
+        }}
+      >
+        <AnimatePresence>
+          {isActive &&
+            cursorTrail.map((cursor, index) => (
+              <motion.div
+                key={cursor.id}
+                initial={{ opacity: 0.8, scale: 1 }}
+                animate={{ opacity: 0, scale: 0.8 }}
+                exit={{ opacity: 0, scale: 0.5 }}
+                transition={{ duration: 0.5, ease: 'easeOut' }}
                 style={{
-                  opacity: 1 - index * 0.1, // Fade out based on position in the trail
+                  position: 'absolute',
+                  left: cursor.x - 10,
+                  top: cursor.y + 5,
                 }}
               >
-                <g fill='none' fillRule='evenodd' transform='translate(10 7)'>
-                  <path
-                    d='m6.148 18.473 1.863-1.003 1.615-.839-2.568-4.816h4.332l-11.379-11.408v16.015l3.316-3.221z'
-                    fill='#fff'
-                  />
-                  <path
-                    d='m6.431 17 1.765-.941-2.775-5.202h3.604l-8.025-8.043v11.188l2.53-2.442z'
-                    fill='#000'
-                  />
-                </g>
-              </svg>
-            </motion.div>
-          ))}
-      </AnimatePresence>
-    </div>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  height='32'
+                  viewBox='0 0 32 32'
+                  width='32'
+                  style={{
+                    opacity: 1 - index * 0.1,
+                  }}
+                >
+                  <g fill='none' fillRule='evenodd' transform='translate(10 7)'>
+                    <path
+                      d='m6.148 18.473 1.863-1.003 1.615-.839-2.568-4.816h4.332l-11.379-11.408v16.015l3.316-3.221z'
+                      fill='#fff'
+                    />
+                    <path
+                      d='m6.431 17 1.765-.941-2.775-5.202h3.604l-8.025-8.043v11.188l2.53-2.442z'
+                      fill='#000'
+                    />
+                  </g>
+                </svg>
+              </motion.div>
+            ))}
+        </AnimatePresence>
+      </div>
+    </>
   )
 }
 
