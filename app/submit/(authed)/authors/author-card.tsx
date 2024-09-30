19cbab62c53c8fda1bc0f96e757381855e7ddac7
Kata Martin <katasm@gmail.com>
2024-09-30 17:10:49 -0400

    Implement draggable interaction

--- app/submit/(authed)/authors/author-card.tsx
+++ app/submit/(authed)/authors/author-card.tsx
@@ -1,6 +1,8 @@
 import { Box, Flex } from 'theme-ui'
 import { useSession } from 'next-auth/react'
 import { useCallback } from 'react'
+import { useDraggable, useDroppable } from '@dnd-kit/core'
+import { CSS } from '@dnd-kit/utilities'
 
 import { usePreprint } from '../../preprint-context'
 import { Author } from '../../../../types/preprint'
@@ -16,6 +18,18 @@ const AuthorCard = ({
 }) => {
   const { data: session } = useSession()
   const { preprint, setPreprint } = usePreprint()
+  const { isOver, setNodeRef: setDroppableNodeRef } = useDroppable({
+    id: author.pk,
+  })
+  const {
+    attributes,
+    listeners,
+    setNodeRef: setDraggableNodeRef,
+    transform,
+    isDragging,
+  } = useDraggable({
+    id: author.pk,
+  })
 
   const handleClick = useCallback(() => {
     updatePreprint(preprint, {
@@ -24,36 +38,44 @@ const AuthorCard = ({
   }, [preprint, author, setPreprint])
 
   return (
-    <Box
-      sx={{
-        variant: 'text.mono',
-        width: '100%',
-        height: 'auto',
-        p: [3, 6, 6, 7],
-        borderColor: 'text',
-        borderWidth: '1px',
-        borderStyle: 'solid',
-        outline: 'none', // use highlight style for focus instead
-      }}
-    >
-      <Flex sx={{ flexDirection: 'column', gap: 1 }}>
-        <Flex sx={{ justifyContent: 'space-between' }}>
-          <Box>
-            {author.first_name} {author.last_name}
-            {author.pk === session?.user?.id ? ' (owner)' : ''}
-          </Box>
-          {removable && (
-            <Link
-              sx={{ variant: 'text.monoCaps', textDecoration: 'none' }}
-              onClick={handleClick}
-            >
-              (X)
-            </Link>
-          )}
+    <Box ref={setDroppableNodeRef}>
+      <Box
+        ref={setDraggableNodeRef}
+        sx={{
+          cursor: 'pointer',
+          variant: 'text.mono',
+          width: '100%',
+          height: 'auto',
+          p: [3, 6, 6, 7],
+          borderColor: 'text',
+          borderWidth: '1px',
+          borderStyle: 'solid',
+          outline: 'none', // use highlight style for focus instead
+          transform: CSS.Translate.toString(transform),
+          background: isOver && !isDragging ? 'background' : 'white',
+        }}
+        {...listeners}
+        {...attributes}
+      >
+        <Flex sx={{ flexDirection: 'column', gap: 1 }}>
+          <Flex sx={{ justifyContent: 'space-between' }}>
+            <Box>
+              {author.first_name} {author.last_name}
+              {author.pk === session?.user?.id ? ' (owner)' : ''}
+            </Box>
+            {removable && (
+              <Link
+                sx={{ variant: 'text.monoCaps', textDecoration: 'none' }}
+                onClick={handleClick}
+              >
+                (X)
+              </Link>
+            )}
+          </Flex>
+          <Box>{author.email}</Box>
+          {author.institution && <Box>{author.institution}</Box>}
         </Flex>
-        <Box>{author.email}</Box>
-        {author.institution && <Box>{author.institution}</Box>}
-      </Flex>
+      </Box>
     </Box>
   )
 }
--- app/submit/(authed)/authors/authors-list.tsx
+++ app/submit/(authed)/authors/authors-list.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { useCallback, useState } from 'react'
+import { Active, DndContext, Over } from '@dnd-kit/core'
+
 import { usePreprint } from '../../preprint-context'
 import { Row } from '../../../../components'
 import AuthorCard from './author-card'
@@ -9,11 +12,36 @@ type Props = {
 }
 const AuthorsList: React.FC<Props> = ({ removable = true }) => {
   const { preprint } = usePreprint()
+  const [authors, setAuthors] = useState(preprint.authors)
+
+  const handleDrag = useCallback(
+    ({ active, over }: { active: Active; over: Over | null }) => {
+      if (over && active.id !== over.id) {
+        setAuthors((prev) => {
+          const [lowIndex, highIndex] = [over.id, active.id]
+            .map((id) => prev.findIndex((author) => author.pk === id))
+            .sort()
+
+          return [
+            ...prev.slice(0, lowIndex),
+            prev[highIndex],
+            prev[lowIndex],
+            ...prev.slice(lowIndex + 1, highIndex),
+            ...prev.slice(highIndex + 1),
+          ]
+        })
+      }
+    },
+    [],
+  )
+
   return (
     <Row columns={[1, 1, 2, 2]} gap={[5, 6, 6, 8]}>
-      {preprint.authors.map((a) => (
-        <AuthorCard key={a.email} author={a} removable={removable} />
-      ))}
+      <DndContext onDragEnd={handleDrag}>
+        {authors.map((a) => (
+          <AuthorCard key={a.email} author={a} removable={removable} />
+        ))}
+      </DndContext>
     </Row>
   )
 }
