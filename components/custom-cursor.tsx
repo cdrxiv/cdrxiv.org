c27cc426f77bdbfc8038533aa0cc0742ce859924
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-10-07 08:25:20 -0700

    hide the custom cursor when the mouse is outside the browser window

--- components/custom-cursor.tsx
+++ components/custom-cursor.tsx
@@ -6,6 +6,7 @@ import { Box, Global } from 'theme-ui'
 const CustomCursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 })
   const [cursorType, setCursorType] = useState('default')
+  const [isVisible, setIsVisible] = useState(true)
 
   useEffect(() => {
     const updatePosition = (e: MouseEvent) => {
@@ -25,12 +26,24 @@ const CustomCursor = () => {
       }
     }
 
+    const handleVisibility = () => {
+      setIsVisible(document.hasFocus())
+    }
+
     window.addEventListener('mousemove', updatePosition)
     window.addEventListener('mouseover', updateCursorType)
+    window.addEventListener('focus', handleVisibility)
+    window.addEventListener('blur', handleVisibility)
+    document.addEventListener('mouseleave', () => setIsVisible(false))
+    document.addEventListener('mouseenter', () => setIsVisible(true))
 
     return () => {
       window.removeEventListener('mousemove', updatePosition)
       window.removeEventListener('mouseover', updateCursorType)
+      window.removeEventListener('focus', handleVisibility)
+      window.removeEventListener('blur', handleVisibility)
+      document.removeEventListener('mouseleave', () => setIsVisible(false))
+      document.removeEventListener('mouseenter', () => setIsVisible(true))
     }
   }, [])
 
@@ -46,25 +59,27 @@ const CustomCursor = () => {
           },
         }}
       />
-      <Box
-        as='div'
-        sx={{
-          position: 'fixed',
-          pointerEvents: 'none',
-          zIndex: 9999,
-          left: 0,
-          top: 0,
-          transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
-          width: '32px',
-          height: '32px',
-          backgroundImage:
-            cursorType === 'hover'
-              ? "url('/cursors/harrow.cur')"
-              : "url('/cursors/arrow_m.cur')",
-          backgroundSize: 'contain',
-          backgroundRepeat: 'no-repeat',
-        }}
-      />
+      {isVisible && (
+        <Box
+          as='div'
+          sx={{
+            position: 'fixed',
+            pointerEvents: 'none',
+            zIndex: 9999,
+            left: 0,
+            top: 0,
+            transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
+            width: '32px',
+            height: '32px',
+            backgroundImage:
+              cursorType === 'hover'
+                ? "url('/cursors/harrow.cur')"
+                : "url('/cursors/arrow_m.cur')",
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat',
+          }}
+        />
+      )}
     </>
   )
 }
