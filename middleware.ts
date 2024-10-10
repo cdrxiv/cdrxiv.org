5444bcf27f9485a52ba2f1b1c457787cf8202e10
Kata Martin <katasm@gmail.com>
2024-10-09 17:07:05 -0700

    Add Plausible route to middleware

--- middleware.ts
+++ middleware.ts
@@ -38,6 +38,6 @@ export const middleware = (
 
 export const config = {
   matcher: [
-    '/((?!api|proxy|_next/static|_next/image|favicon.ico|icon.png|sitemap.xml|robots.txt).*)',
+    '/((?!api|proxy/api/event|_next/static|_next/image|favicon.ico|icon.png|sitemap.xml|robots.txt).*)',
   ],
 }
