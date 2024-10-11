0757f6c0957cff6bc10c1498deaefac96a1e2713
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-10-11 17:26:21 -0500

    basic team mock

--- app/about/team/team.tsx
+++ app/about/team/team.tsx
@@ -1,25 +1,149 @@
 'use client'
 
 import { Box } from 'theme-ui'
+import Image, { StaticImageData } from 'next/image'
+import { Link, Row, Column } from '../../../components'
 
-const team = [
+import tk from './images/tk.jpg'
+
+type Affiliate = {
+  name: string
+  affiliation: string
+  affiliationLink: string
+}
+
+type EditorialBoardMember = {
+  name: string
+  role: string
+  image: StaticImageData
+  affiliation: string
+  affiliationLink: string
+  link: string
+}
+
+const editorialBoard: EditorialBoardMember[] = [
   {
-    name: 'name 1',
-    bio: 'bio 1',
+    name: 'Tyler Kukla',
+    role: 'Editor-in-Chief',
+    image: tk,
+    affiliation: 'CarbonPlan',
+    affiliationLink: 'https://carbonplan.org/',
+    link: 'https://tylerkukla.com/',
   },
   {
-    name: 'name 2',
-    bio: 'bio 2',
+    name: 'Tyler Kukla',
+    role: 'Editor-in-Chief',
+    image: tk,
+    affiliation: 'CarbonPlan',
+    affiliationLink: 'https://carbonplan.org/',
+    link: 'https://tylerkukla.com/',
+  },
+  {
+    name: 'Tyler Kukla',
+    role: 'Editor-in-Chief',
+    image: tk,
+    affiliation: 'CarbonPlan',
+    affiliationLink: 'https://carbonplan.org/',
+    link: 'https://tylerkukla.com/',
+  },
+  {
+    name: 'Tyler Kukla',
+    role: 'Editor-in-Chief',
+    image: tk,
+    affiliation: 'CarbonPlan',
+    affiliationLink: 'https://carbonplan.org/',
+    link: 'https://tylerkukla.com/',
+  },
+  {
+    name: 'Tyler Kukla',
+    role: 'Editor-in-Chief',
+    image: tk,
+    affiliation: 'CarbonPlan',
+    affiliationLink: 'https://carbonplan.org/',
+    link: 'https://tylerkukla.com/',
+  },
+]
+
+const affiliates: Affiliate[] = [
+  {
+    name: 'Tyler Kukla',
+    affiliation: 'CarbonPlan',
+    affiliationLink: 'https://carbonplan.org/',
+  },
+  {
+    name: 'Tyler Kukla',
+    affiliation: 'CarbonPlan',
+    affiliationLink: 'https://carbonplan.org/',
+  },
+  {
+    name: 'Tyler Kukla',
+    affiliation: 'CarbonPlan',
+    affiliationLink: 'https://carbonplan.org/',
+  },
+  {
+    name: 'Tyler Kukla',
+    affiliation: 'CarbonPlan',
+    affiliationLink: 'https://carbonplan.org/',
   },
 ]
 
 const Team: React.FC = () => {
   return (
     <Box>
-      {team.map(({ name, bio }) => (
-        <Box key={name}>
-          <Box as='h2'>{name}</Box>
-          <Box>{bio}</Box>
+      <Box as='h2' sx={{ mb: 4 }}>
+        Editorial Board
+      </Box>
+      <Row columns={[6, 6, 8, 8]}>
+        {editorialBoard.map(
+          (
+            { name, role, link, image, affiliation, affiliationLink },
+            index,
+          ) => (
+            <Column
+              key={name + index}
+              start={[index % 2 === 0 ? 1 : 4, index % 2 === 0 ? 1 : 5]}
+              width={[3, 3, 4, 4]}
+              sx={{ mb: 6 }}
+            >
+              <Image
+                src={image}
+                alt={name}
+                style={{ width: '100%', height: 'auto' }}
+              />
+              <Box as='h3'>
+                {name}
+                <Link target='_blank' href={link} sx={{ ml: 2 }}>
+                  {'→'}
+                </Link>
+              </Box>
+              <Box variant='text.monoCaps'>{role}</Box>
+              <Box>
+                <Link
+                  href={affiliationLink}
+                  target='_blank'
+                  variant='text.mono'
+                >
+                  ({affiliation})
+                </Link>
+              </Box>
+            </Column>
+          ),
+        )}
+      </Row>
+      <Box as='h2' sx={{ mb: 2, mt: 4 }}>
+        Affiliates
+      </Box>
+      {affiliates.map(({ name, affiliation, affiliationLink }, index) => (
+        <Box key={name + index} variant='text.mono'>
+          {name}
+          <Link
+            href={affiliationLink}
+            target='_blank'
+            variant='text.mono'
+            sx={{ ml: 2 }}
+          >
+            ({affiliation})
+          </Link>
         </Box>
       ))}
     </Box>
