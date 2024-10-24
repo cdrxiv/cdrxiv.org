75f557a5549c4301bdfe764399083c5d6882a726
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-10-24 12:51:13 -0500

    improve submissions view

--- app/(userSection)/submissions/(index)/preprints-table.tsx
+++ app/(userSection)/submissions/(index)/preprints-table.tsx
@@ -29,7 +29,7 @@ const PreprintsTable: React.FC<PreprintsTableProps> = ({ preprints, date }) => {
     return <Box sx={{ variant: 'text.monoCaps', my: 3 }}>None</Box>
   }
   return (
-    <>
+    <Box as='ul' sx={{ p: 0 }}>
       <Row
         columns={[6, 6, 8, 8]}
         sx={{ alignItems: 'center', flexWrap: 'wrap', mt: 3, mb: 2 }}
@@ -52,90 +52,92 @@ const PreprintsTable: React.FC<PreprintsTableProps> = ({ preprints, date }) => {
       {preprints
         .sort((a: Preprint, b: Preprint) => getDateValue(b) - getDateValue(a))
         .map((preprint, i) => (
-          <NextLink
-            href={`/submissions/edit/${preprint.pk}`}
-            key={preprint.pk}
-            style={{ textDecoration: 'none' }}
-          >
-            <Box
-              sx={{
-                color: 'text',
-                borderTop: '1px solid',
-                borderBottom: i === preprints.length - 1 ? '1px solid' : 0,
-                borderColor: 'listBorderGrey',
-                py: 4,
-                ':hover': {
-                  cursor: 'pointer',
-                  bg: 'white',
-                  '#title': {
-                    color: 'blue',
-                  },
-                },
-              }}
+          <Box as='li' key={preprint.pk} sx={{ listStyle: 'none' }}>
+            <NextLink
+              href={`/submissions/edit/${preprint.pk}`}
+              key={preprint.pk}
+              style={{ textDecoration: 'none' }}
             >
-              <Row
-                columns={[6, 6, 8, 8]}
-                sx={{ alignItems: 'center', flexWrap: 'wrap' }}
+              <Box
+                sx={{
+                  color: 'text',
+                  borderTop: '1px solid',
+                  borderBottom: i === preprints.length - 1 ? '1px solid' : 0,
+                  borderColor: 'listBorderGrey',
+                  py: 4,
+                  ':hover': {
+                    cursor: 'pointer',
+                    bg: 'white',
+                    '#title': {
+                      color: 'blue',
+                    },
+                  },
+                }}
               >
-                <Column start={[1, 1, 1, 1]} width={[6, 6, 4, 4]}>
-                  <Box
-                    id='title'
-                    sx={{
-                      variant: 'text.body',
-                      color: ['blue', 'blue', 'inherit', 'inherit'],
-                      mb: [4, 4, 0, 0],
-                    }}
-                  >
-                    {preprint.title}
-                  </Box>
-                </Column>
-                <Column start={[1, 1, 5, 5]} width={[6, 6, 4, 4]}>
-                  <Flex
-                    sx={{
-                      flexDirection: [
-                        'row-reverse',
-                        'row-reverse',
-                        'row',
-                        'row',
-                      ],
-                      justifyContent: 'space-between',
-                      alignItems: 'baseline',
-                    }}
-                  >
+                <Row
+                  columns={[6, 6, 8, 8]}
+                  sx={{ alignItems: 'center', flexWrap: 'wrap' }}
+                >
+                  <Column start={[1, 1, 1, 1]} width={[6, 6, 4, 4]}>
                     <Box
+                      id='title'
                       sx={{
-                        variant: 'text.monoCaps',
-                        width: 'fit-content',
+                        variant: 'text.body',
+                        color: ['blue', 'blue', 'inherit', 'inherit'],
+                        mb: [4, 4, 0, 0],
                       }}
                     >
-                      {preprint[date]
-                        ? formatDate(new Date(preprint[date]))
-                        : null}
+                      {preprint.title}
                     </Box>
-                    <Flex sx={{ gap: 2, flexWrap: 'wrap' }}>
-                      {submissionTypes(preprint).map((badge) => (
-                        <Badge key={badge.label} color={badge.color}>
-                          {badge.label}
-                        </Badge>
-                      ))}
-                      {submissionTypes(preprint).length === 0 && (
-                        <Box
-                          sx={{
-                            variant: 'text.monoCaps',
-                            color: 'listBorderGrey',
-                          }}
-                        >
-                          Not labeled
-                        </Box>
-                      )}
+                  </Column>
+                  <Column start={[1, 1, 5, 5]} width={[6, 6, 4, 4]}>
+                    <Flex
+                      sx={{
+                        flexDirection: [
+                          'row-reverse',
+                          'row-reverse',
+                          'row',
+                          'row',
+                        ],
+                        justifyContent: 'space-between',
+                        alignItems: 'baseline',
+                      }}
+                    >
+                      <Box
+                        sx={{
+                          variant: 'text.monoCaps',
+                          width: 'fit-content',
+                        }}
+                      >
+                        {preprint[date]
+                          ? formatDate(new Date(preprint[date]))
+                          : null}
+                      </Box>
+                      <Flex sx={{ gap: 2, flexWrap: 'wrap' }}>
+                        {submissionTypes(preprint).map((badge) => (
+                          <Badge key={badge.label} color={badge.color}>
+                            {badge.label}
+                          </Badge>
+                        ))}
+                        {submissionTypes(preprint).length === 0 && (
+                          <Box
+                            sx={{
+                              variant: 'text.monoCaps',
+                              color: 'listBorderGrey',
+                            }}
+                          >
+                            Not labeled
+                          </Box>
+                        )}
+                      </Flex>
                     </Flex>
-                  </Flex>
-                </Column>
-              </Row>
-            </Box>
-          </NextLink>
+                  </Column>
+                </Row>
+              </Box>
+            </NextLink>
+          </Box>
         ))}
-    </>
+    </Box>
   )
 }
 
