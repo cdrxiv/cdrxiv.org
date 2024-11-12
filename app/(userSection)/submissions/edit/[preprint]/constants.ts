fd6e7252cb5b038af8e5dcdba1c52b54abd7c539
Kata Martin <katasm@gmail.com>
2024-11-12 15:42:17 -0500

    Update revision form text

--- app/(userSection)/submissions/edit/[preprint]/constants.ts
+++ app/(userSection)/submissions/edit/[preprint]/constants.ts
@@ -1,13 +1,14 @@
 export const UPDATE_TYPE_LABELS = {
-  metadata_correction: 'Metadata correction',
-  correction: 'Text correction',
   version: 'New version',
+  correction: 'Text correction',
+  metadata_correction: 'Metadata correction',
 }
 
 export const UPDATE_TYPE_DESCRIPTIONS = {
-  metadata_correction: 'You can only make changes to the metadata.',
-  correction:
-    'This is a small update. You must upload a new file, and additionally, you can update the metadata.',
   version:
-    'A large update. You must upload a new file, and additionally, you can update the metadata.',
+    'A significant update. You must upload a new file and you may update the other fields below. Use this option for updates after peer review.',
+  correction:
+    'A minor update to the text, usually to fix small errors. You must upload a new file and you may update the other fields below.',
+  metadata_correction:
+    'Use this option to update any of the fields below, but not to upload a new file.',
 }
