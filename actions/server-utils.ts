d6d0c2a858f8f65bae4d0fcb69c4e4d46ed65c43
Kata Martin <katasm@gmail.com>
2024-11-20 16:50:43 -0500

    Set up basic alerting

--- actions/server-utils.ts
+++ actions/server-utils.ts
@@ -1,5 +1,8 @@
 'use server'
 
+import { getToken } from 'next-auth/jwt'
+import { headers, cookies } from 'next/headers'
+
 export async function catchActionErrors<A extends any[], R>(
   serverAction: (...args: A) => Promise<R>,
   ...args: A
@@ -9,6 +12,33 @@ export async function catchActionErrors<A extends any[], R>(
     result = await serverAction(...args)
     return { result }
   } catch (e: any) {
+    const token = await getToken({
+      req: {
+        headers: headers(),
+        cookies: cookies(),
+      } as any,
+      secret: process.env.NEXTAUTH_SECRET,
+    })
+
+    await fetch(process.env.NEXT_PUBLIC_SLACK_WEBHOOK_URL as string, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        error: e.message ?? 'None provided',
+        user_id: token?.user?.id ?? 'null',
+        environment:
+          process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+            ? 'production'
+            : 'staging',
+        domain:
+          process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+            ? 'cdrxiv.org'
+            : 'staging.cdrxiv.org',
+      }),
+    })
+
     return {
       error: e.message ?? 'Error saving changes.',
     }
