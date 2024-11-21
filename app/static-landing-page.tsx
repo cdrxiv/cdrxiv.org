79ef1699d9da5351002ba06e2af7d4d8f7dbf585
Kata Martin <katasm@gmail.com>
2024-11-21 16:35:54 -0500

    Tweak static landing page positioning

--- app/static-landing-page.tsx
+++ app/static-landing-page.tsx
@@ -7,14 +7,14 @@ import { Column, Link, Row } from '../components'
 const StaticLandingPage = () => {
   return (
     <>
-      <Row columns={[6, 8, 12, 12]} sx={{ my: [4, 4, 8, 8] }}>
-        <Column start={[1, 1, 2, 2]} width={[6, 6, 5, 5]} sx={{ mb: 9 }}>
+      <Row columns={[6, 8, 12, 12]} sx={{ my: [4, 4, 9, 9] }}>
+        <Column start={[1, 1, 3, 3]} width={[6, 6, 5, 5]} sx={{ mb: 9 }}>
           <Box sx={{ variant: 'styles.h1' }} as='h1'>
             Preprints and Data for Carbon Dioxide Removal
           </Box>
         </Column>
         <Column
-          start={[1, 1, 2, 2]}
+          start={[1, 1, 3, 3]}
           width={[6, 6, 6, 6]}
           sx={{ mb: [0, 0, 8, 8] }}
         >
