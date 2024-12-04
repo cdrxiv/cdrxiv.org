060292c829930e28e6917b4d00b60e0c323872aa
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-12-04 13:00:32 -0600

    dynamic triangle, title, fix delay and spacing

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,47 +3,27 @@
 import { Box, Flex } from 'theme-ui'
 import { usePathname } from 'next/navigation'
 
-const logo = `
-4
-404
-40404
-4040404
-404040404
-40404040404
-4040404040404
-404040404040404
-4040404040404040
-404040404040404040
-40404040404040404040
-4040404040404040404040
-404040404040404040404040
-40404040404040404040404040
-`
-
 const delayFactor = 0.0015
 
 export default function NotFound() {
   const pathname = usePathname()
   return (
     <>
-      <title>Not Found - CDRXIV</title>
+      <title>404 - CDRXIV</title>
       <Flex
-        variant='text.mono'
+        variant='text.monoCaps'
         sx={{
+          flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
-          mb: 150,
-          flexDirection: 'column',
-          display: 'flex',
-          height: '70vh',
+          height: '80vh',
         }}
       >
-        <Box
+        <Flex
           sx={{
-            whiteSpace: 'pre',
+            gap: '2px',
             lineHeight: 1,
             fontSize: [2, 2, 3, 3],
-            display: 'flex',
             flexDirection: 'column',
             '@keyframes fadeOut': {
               from: { opacity: 1 },
@@ -55,44 +35,43 @@ export default function NotFound() {
             },
           }}
         >
-          {logo.split('\n').map((line, lineIndex) => (
-            <Box key={`line-${lineIndex}`}>
-              {line.split('').map((char, charIndex) => (
-                <Box
-                  key={`${lineIndex}-${charIndex}`}
-                  sx={{
-                    display: 'inline-block',
-                    opacity: 1,
-                    animation: `fadeOut 0.01s forwards`,
-                    animationDelay: `${
-                      (lineIndex * line.length + charIndex) * delayFactor
-                    }s`,
-                  }}
-                >
-                  {char}
-                </Box>
-              ))}
-            </Box>
-          ))}
-          <Box
-            sx={{
-              variant: 'text.monoCaps',
-              fontSize: 4,
-              mt: 2,
-              opacity: 0.3,
-              textAlign: 'center',
-              animation: 'fadeIn 0.01s  forwards',
-              animationDelay: `${
-                logo.split('\n').reduce((acc, line) => acc + line.length, 0) *
-                delayFactor *
-                2
-              }s`,
-            }}
-          >
-            {pathname.match(/\/preprint\/\d+/)
-              ? 'Preprint not found'
-              : 'Page not found'}
-          </Box>
+          {Array(14)
+            .fill(null)
+            .map((_, lineIndex) => (
+              <Flex key={lineIndex}>
+                {Array(1 + lineIndex * 2)
+                  .fill(null)
+                  .map((_, charIndex) => (
+                    <Box
+                      key={charIndex}
+                      sx={{
+                        opacity: 1,
+                        animation: `fadeOut 0.01s forwards`,
+                        animationDelay: `${
+                          (lineIndex * (1 + lineIndex * 2) + charIndex) *
+                          delayFactor
+                        }s`,
+                      }}
+                    >
+                      {charIndex % 2 === 0 ? '4' : '0'}
+                    </Box>
+                  ))}
+              </Flex>
+            ))}
+        </Flex>
+        <Box
+          sx={{
+            fontSize: 4,
+            mt: 4,
+            opacity: 0.3,
+            textAlign: 'center',
+            animation: 'fadeIn 0.01s forwards',
+            animationDelay: `${(13 * 27 + 26) * delayFactor}s`,
+          }}
+        >
+          {pathname.match(/\/preprint\/\d+/)
+            ? 'Preprint not found'
+            : 'Page not found'}
         </Box>
       </Flex>
     </>
