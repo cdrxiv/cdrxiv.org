6f775cb02160df96add86b504df62bfdcd34ed0d
Kata Martin <katasm@gmail.com>
2024-12-04 12:47:47 -0500

    Set up basic withdrawal request form

---
+++ app/(userSection)/submissions/withdraw/[preprint]/page.tsx
@@ -0,0 +1,25 @@
+import { redirect } from 'next/navigation'
+
+import WithdrawForm from './withdraw-form'
+import { fetchWithToken } from '../../../../../actions/server-utils'
+
+export const metadata = {
+  title: 'Withdraw Submission – CDRXIV',
+}
+
+const Page = async ({ params }: { params: { preprint: string } }) => {
+  const preprintRes = await fetchWithToken(
+    `${process.env.NEXT_PUBLIC_JANEWAY_URL}/api/user_preprints/${params.preprint}`,
+  )
+
+  if (preprintRes.status !== 200) {
+    redirect(
+      `/account?signOut=true&callbackUrl=/submissions/withdraw/${params.preprint}`,
+    )
+  }
+  const preprint = await preprintRes.json()
+
+  return <WithdrawForm preprint={preprint} />
+}
+
+export default Page
