b50ab3f6b15a36e42127c3f554aad92a543fce2a
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-12-17 14:00:07 -0600

    lower cache age for testing

--- app/preprint/[id]/opengraph-image.tsx
+++ app/preprint/[id]/opengraph-image.tsx
@@ -198,7 +198,7 @@ export default async function Image({ params }: { params: { id: string } }) {
       ...size,
       fonts,
       headers: {
-        'Cache-Control': 'public, max-age=86400',
+        'Cache-Control': 'public, max-age=10',
       },
     },
   )
