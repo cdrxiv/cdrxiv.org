a403ab5456c32c7abc240ad508a3260c8c9d579e
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-01-23 15:27:02 -0600

    add people/images and rework flow

--- app/about/team/team.tsx
+++ app/about/team/team.tsx
@@ -5,79 +5,108 @@ import Image, { StaticImageData } from 'next/image'
 import { Link, Row, Column } from '../../../components'
 
 import tk from './images/tk.jpg'
+import jp from './images/jp.png'
+import fc from './images/fc.png'
+import ga from './images/ga.png'
 
 type Affiliate = {
   name: string
   affiliation: string
-  affiliationLink: string
 }
 
-type EditorialBoardMember = {
+type AdvisoryBoard = {
   name: string
   role: string
   image: StaticImageData
   affiliation: string
-  affiliationLink: string
 }
 
-const editorialBoard: EditorialBoardMember[] = [
+type ContentTeam = {
+  name: string
+  role: string
+  image: StaticImageData
+  affiliation: string
+}
+
+const contentTeam: ContentTeam[] = [
   {
     name: 'Tyler Kukla',
-    role: 'Editor-in-Chief',
+    role: 'Content Curation Lead',
     image: tk,
     affiliation: 'CarbonPlan',
-    affiliationLink: 'https://carbonplan.org/',
+  },
+]
+
+const editorialBoard: AdvisoryBoard[] = [
+  {
+    name: 'Jennifer Pett-Ridge',
+    role: 'Senior Staff Scientist',
+    image: jp,
+    affiliation: 'Lawrence Livermore National Laboratory',
   },
   {
-    name: 'Tyler Kukla',
-    role: 'Editor-in-Chief',
+    name: 'Simon Nicholson',
+    role: 'Co-Director',
     image: tk,
-    affiliation: 'CarbonPlan',
-    affiliationLink: 'https://carbonplan.org/',
+    affiliation:
+      'Institute for Responsible Carbon Removal, American University',
   },
   {
-    name: 'Tyler Kukla',
-    role: 'Editor-in-Chief',
-    image: tk,
+    name: 'Freya Chay',
+    role: 'Carbon Removal Program Lead',
+    image: fc,
     affiliation: 'CarbonPlan',
-    affiliationLink: 'https://carbonplan.org/',
   },
   {
-    name: 'Tyler Kukla',
-    role: 'Editor-in-Chief',
-    image: tk,
-    affiliation: 'CarbonPlan',
-    affiliationLink: 'https://carbonplan.org/',
+    name: 'Grace Andrews',
+    role: 'Founder & Executive Director',
+    image: ga,
+    affiliation: 'Hourglass Climate',
   },
   {
-    name: 'Tyler Kukla',
-    role: 'Editor-in-Chief',
+    name: 'Sam Hindle',
+    role: 'tk',
     image: tk,
-    affiliation: 'CarbonPlan',
-    affiliationLink: 'https://carbonplan.org/',
+    affiliation: 'BioRXIV',
   },
 ]
 
 const affiliates: Affiliate[] = [
   {
-    name: 'Tyler Kukla',
-    affiliation: 'CarbonPlan',
-    affiliationLink: 'https://carbonplan.org/',
+    name: 'Katherine Almquist',
+    affiliation: 'Northwestern University',
   },
   {
-    name: 'Tyler Kukla',
-    affiliation: 'CarbonPlan',
-    affiliationLink: 'https://carbonplan.org/',
+    name: 'Jonah Bernstein-Schalet',
+    affiliation: 'Mati Carbon',
   },
   {
-    name: 'Tyler Kukla',
-    affiliation: 'CarbonPlan',
-    affiliationLink: 'https://carbonplan.org/',
+    name: 'Kenzo Esquivel',
+    affiliation: 'Lawrence Livermore National Laboratory',
   },
   {
-    name: 'Tyler Kukla',
-    affiliation: 'CarbonPlan',
-    affiliationLink: 'https://carbonplan.org/',
+    name: 'Evan Ramos',
+    affiliation: 'University of Pittsburgh',
+  },
+  {
+    name: 'Alexandra Ringsby',
+    affiliation: 'Stanford University',
+  },
+  {
+    name: 'Brian Rogers',
+    affiliation: 'Stanford University',
+  },
+  {
+    name: 'Mary Margaret Stoll',
+    affiliation: 'University of Washington',
+  },
+  {
+    name: 'Allegra Tashjian',
+    affiliation: 'Northwestern University',
+  },
+  {
+    name: 'Trent Thomas',
+    affiliation: 'University of Washington',
   },
 ]
 
@@ -85,53 +114,53 @@ const Team: React.FC = () => {
   return (
     <Box>
       <Box as='h2' sx={{ mb: 4 }}>
-        Editorial board
+        Advisory board
       </Box>
-      <Row columns={[6, 6, 8, 9]}>
-        {editorialBoard.map(
-          ({ name, role, image, affiliation, affiliationLink }, index) => (
-            <Column
-              key={name + index}
-              start={[
-                index % 2 === 0 ? 1 : 4,
-                index % 2 === 0 ? 1 : 4,
-                index % 2 === 0 ? 1 : 5,
-                (index % 3) * 3 + 1,
-              ]}
-              width={[3, 3, 4, 3]}
-              sx={{ mb: 6 }}
-            >
-              <Image
-                src={image}
-                alt={name}
-                style={{ width: '100%', height: 'auto' }}
-              />
-              <Box sx={{ fontSize: [3, 3, 4, 4] }}>{name}</Box>
-              <Box variant='text.monoCaps'>{role}</Box>
-              <Box>
-                <Link
-                  href={affiliationLink}
-                  target='_blank'
-                  variant='text.mono'
-                >
-                  {affiliation}
-                </Link>
-              </Box>
-            </Column>
-          ),
-        )}
+      <Row columns={[6, 6, 6, 9]}>
+        {editorialBoard.map(({ name, role, image, affiliation }, index) => (
+          <Column
+            key={name + index}
+            start={[
+              index % 2 === 0 ? 1 : 4,
+              index % 2 === 0 ? 1 : 4,
+              index % 2 === 0 ? 1 : 4,
+              (index % 3) * 3 + 1,
+            ]}
+            width={[3, 3, 3, 3]}
+            sx={{ mb: 6 }}
+          >
+            <Image
+              src={image}
+              alt={name}
+              style={{ width: '100%', height: 'auto' }}
+            />
+            <Box sx={{ fontSize: [2, 2, 3, 3] }}>{name}</Box>
+            <Box variant='text.mono'>
+              {role}, {affiliation}
+            </Box>
+          </Column>
+        ))}
       </Row>
-      <Box as='h2' sx={{ mb: 2, mt: 4 }}>
-        Affiliates
+
+      <Box as='h2' sx={{ mb: 2, mt: -4 }}>
+        Content team
       </Box>
-      {affiliates.map(({ name, affiliation, affiliationLink }, index) => (
-        <Box key={name + index} sx={{ mb: 3 }}>
+      {contentTeam.map(({ name, role, image, affiliation }, index) => (
+        <Box key={name + index} sx={{ mb: 1 }}>
           <Box>{name}</Box>
           <Box sx={{ variant: 'text.mono' }}>
-            <Link href={affiliationLink} variant='text.mono'>
-              {affiliation}
-            </Link>
+            {role}, {affiliation}
           </Box>
+        </Box>
+      ))}
+
+      <Box as='h2' sx={{ mb: 2, mt: 4 }}>
+        Affiliate expert screeners
+      </Box>
+      {affiliates.map(({ name, affiliation }, index) => (
+        <Box key={name + index} sx={{ mb: 1 }}>
+          <Box>{name}</Box>
+          <Box sx={{ variant: 'text.mono' }}>{affiliation}</Box>
         </Box>
       ))}
     </Box>
