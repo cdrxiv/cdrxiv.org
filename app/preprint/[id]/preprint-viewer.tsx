bec3b6ed89aa4fff8562dc6eca8bdd71a28bff05
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-01-28 14:41:09 -0600

    move polyfill to client component

--- app/preprint/[id]/preprint-viewer.tsx
+++ app/preprint/[id]/preprint-viewer.tsx
@@ -21,6 +21,23 @@ import { Deposition } from '../../../types/zenodo'
 import { fetchDataDeposition, fetchPreprintIdentifier } from '../../../actions'
 import ErrorOrTrack from './error-or-track'
 
+// Polyfill for Promise.withResolvers
+if (typeof Promise.withResolvers !== 'function') {
+  Promise.withResolvers = function <T>(): {
+    promise: Promise<T>
+    resolve: (value: T | PromiseLike<T>) => void
+    reject: (reason?: any) => void
+  } {
+    let resolve!: (value: T | PromiseLike<T>) => void
+    let reject!: (reason?: any) => void
+    const promise = new Promise<T>((res, rej) => {
+      resolve = res
+      reject = rej
+    })
+    return { promise, resolve, reject }
+  }
+}
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
 
 const PreprintViewer = ({
