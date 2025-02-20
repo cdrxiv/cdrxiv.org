ddd5f339e9e573bcd43d4a38f2596389f3cc4a7b
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-02-20 17:33:22 -0600

    add robots and sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,60 @@
+import { MetadataRoute } from 'next'
+import { fetchWithAlerting } from '../actions/server-utils'
+import { Preprints } from '../types/preprint'
+
+export const dynamic = 'force-dynamic'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = 'https://cdrxiv.org'
+
+  const staticRoutes = [
+    {
+      url: `${baseUrl}/`,
+      changeFrequency: 'weekly' as const,
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      changeFrequency: 'monthly' as const,
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/about/team`,
+      changeFrequency: 'monthly' as const,
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/about/faq`,
+      changeFrequency: 'monthly' as const,
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/about/scope`,
+      changeFrequency: 'monthly' as const,
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/account`,
+      changeFrequency: 'monthly' as const,
+      priority: 0.5,
+    },
+  ]
+
+  const apiUrl = `${process.env.NEXT_PUBLIC_JANEWAY_URL}/api/published_preprints/`
+  const res = await fetchWithAlerting(apiUrl, { next: { revalidate: 180 } })
+  const data = await res.json()
+  const preprints: Preprints = data?.results ?? []
+  const preprintSitemap: MetadataRoute.Sitemap = preprints.map(
+    (preprint: any) => {
+      return {
+        url: `${baseUrl}/preprint/${preprint.pk}`,
+        lastModified: new Date(preprint.date_published),
+        changeFrequency: 'yearly' as const,
+        priority: 0.9,
+      }
+    },
+  )
+
+  const sitemap = [...staticRoutes, ...preprintSitemap]
+  return sitemap
+}
