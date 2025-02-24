95eb877fba86a6d745e6f21028cab37d6993f29a
Kata Martin <katasm@gmail.com>
2025-02-24 14:45:58 -0500

    Set up initial rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,18 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  async rewrites() {
+    return [
+      {
+        rewrites: [
+          {
+            source: '/repository/object/:slug*',
+            destination: `${process.env.NEXT_PUBLIC_JANEWAY_URL}/repository/object/:slug*`,
+          },
+        ],
+      },
+    ]
+  },
 }
 
 const withMDX = createMDX({})
