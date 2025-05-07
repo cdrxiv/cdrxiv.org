ddf7ddf7e367b49d25d5b23461c4364501406d97
Kata Martin <katasm@gmail.com>
2025-05-07 12:36:21 -0400

    Remove subjects sorting

--- app/subjects-context.tsx
+++ app/subjects-context.tsx
@@ -20,12 +20,6 @@ export const SubjectsProvider: React.FC<SubjectsProviderProps> = ({
   )
 }
 
-const TOPIC_AREAS = {
-  'Accounting and verification': true,
-  'Experiments and field trials': true,
-  Modeling: true,
-  'Social science and political economy': true,
-}
 export const useSubjects = () => {
   const subjects = useContext(SubjectsContext)
 
@@ -33,15 +27,5 @@ export const useSubjects = () => {
     throw new Error('Tried to use subjects before initiated or provided')
   }
 
-  const sortedSubjects = useMemo(() => {
-    const topics = subjects.filter((s) => TOPIC_AREAS.hasOwnProperty(s.name))
-    const pathways = subjects.filter(
-      (s) => !TOPIC_AREAS.hasOwnProperty(s.name) && s.name !== 'Other',
-    )
-    const other = subjects.filter((s) => s.name === 'Other')
-
-    return [...topics, ...pathways, ...other]
-  }, [subjects])
-
-  return sortedSubjects
+  return subjects
 }
